c3c23b239a3b8252ba455cb71d140425ce78da38
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-07-28 14:50:45 -0300

    requirements.txt en raiz

--- Sistema_Gestion_LR/backend/app/core/config.py
+++ Sistema_Gestion_LR/backend/app/core/config.py
@@ -3,4 +3,7 @@ import os
 # URL de conexión a la base de datos
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "mysql+pymysql://root:admin@localhost/software_gestion_db")
+    "postgresql://postgres.jpeibrtqlktbrhwemnbi:Laroca6729@aws-0-sa-east-1.pooler.supabase.com:6543/postgres")
+
+
+
