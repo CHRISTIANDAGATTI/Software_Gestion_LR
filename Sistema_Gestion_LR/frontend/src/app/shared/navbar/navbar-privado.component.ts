44409b58b6a0d49e8be6801813561f54123c4bdb
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-07-28 23:34:07 -0300

    cerrar sesion

--- Sistema_Gestion_LR/frontend/src/app/shared/navbar/navbar-privado.component.ts
+++ Sistema_Gestion_LR/frontend/src/app/shared/navbar/navbar-privado.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-privado',
   templateUrl: './navbar-privado.component.html',
   styleUrls: ['./navbar-privado.component.scss']
 })
-export class NavbarPrivadoComponent { }
+export class NavbarPrivadoComponent {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async cerrarSesion() {
+    await this.authService.signOut();
+    localStorage.clear();
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+    window.location.reload();
+  }
+}
