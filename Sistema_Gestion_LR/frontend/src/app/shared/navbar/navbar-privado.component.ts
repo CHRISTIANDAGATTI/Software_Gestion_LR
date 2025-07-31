04ab6d5cfc184c526ce1f82a2555169cc9ce06ba
CHRISTIANDAGATTI <dagattichristian@gmail.com>
2025-07-31 03:19:00 -0300

    Usuario y Empresa en Navbar

--- Sistema_Gestion_LR/frontend/src/app/shared/navbar/navbar-privado.component.ts
+++ Sistema_Gestion_LR/frontend/src/app/shared/navbar/navbar-privado.component.ts
@@ -8,7 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar-privado.component.scss']
 })
 export class NavbarPrivadoComponent {
-  constructor(private authService: AuthService, private router: Router) {}
+  empresaNombre: string = '';
+  username: string = '';
+  isAdmin: boolean = false;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.cargarDatosUsuario();
+  }
+
+  async cargarDatosUsuario() {
+    const user = await this.authService.getUser();
+    if (user) {
+      this.username = user.user_metadata?.username || user.email;
+      this.empresaNombre = user.user_metadata?.empresa_nombre || '';
+      this.isAdmin = user.user_metadata?.rol === 'admin';
+    }
+  }
 
   async cerrarSesion() {
     await this.authService.signOut();
